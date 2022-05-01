5b6da7cd496a3e976b971544cc42d23c15cadc44
CmmSheparD <ustynow.gleb@gmail.com>
2022-05-01 04:05:19 +0300

    feat: Internal logic change for DirectorRecord and Fetcher.

--- src/main/kotlin/core/storage/records/DirectoryRecord.kt
+++ src/main/kotlin/core/storage/records/DirectoryRecord.kt
@@ -1,12 +1,14 @@
 package core.storage.records
 
-class DirectoryRecord(name: String, hash: String, _children: List<Record>) : Record(name, hash) {
-    private val childrenList: MutableList<Record> = _children.toMutableList()
-    val children: List<Record>
+import core.hash.SHA1Hash
+
+class DirectoryRecord(name: String, hash: String, _children: List<Record> = emptyList()) : Record(name, hash) {
+    var children = _children.sortedBy { it.name }
         get() {
-            if (!isFetched) error("Accessing not fetched record.")
-            return childrenList
+            return if (!isFetched) error("Accessing not fetched record.")
+            else field
         }
+        private set
     var isFetched = children.isNotEmpty()
         private set
 
@@ -14,26 +16,43 @@ class DirectoryRecord(name: String, hash: String, _children: List<Record>) : Rec
 
     class Fetcher : DirectoryRecordBuilder {
         private var record: DirectoryRecord? = null
+        private var children = mutableListOf<Record>()
 
         override fun reset() {
             record = null
         }
 
-        fun startFetch(record: DirectoryRecord) {
+        fun startFetching(record: DirectoryRecord) {
             check(this.record == null)
             this.record = record
         }
 
         override fun addChild(record: Record) {
-            this.record!!.childrenList.add(record)
+            check(this.record?.isFetched == false)
+            children.add(record)
+        }
+
+        override fun addChildren(records: List<Record>) {
+            for (record in records) addChild(record)
         }
 
         override fun getResult(): DirectoryRecord {
+            check(record != null)
+            check(children.isNotEmpty())
+            children.sortBy { it.name }
+            checkConsistency()
+            record!!.children = children.sortedBy { it.name }
             record!!.isFetched = true
-            record!!.childrenList.sortBy { it.name }
             return record!!
+        }
+
+        private fun checkConsistency() {
+            if (SHA1Hash.calculate(children.joinToString(CANONICAL_RECORD_LINE_SEPARATOR)) != record!!.hash)
+                throw FetchingConsistencyException()
         }
     }
 }
+
+class FetchingConsistencyException : Exception("Fetched content's hash differs from prefetched hash.")
 
 const val CANONICAL_RECORD_LINE_SEPARATOR = "\n"
